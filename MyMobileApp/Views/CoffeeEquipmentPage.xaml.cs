21cfcf53370fa84d7fe4dbd2ac80574027ba46b7
Angel Vukov <samsoft@abv.bg>
2022-03-18 16:20:06 +0200

    First

---
+++ MyMobileApp/Views/CoffeeEquipmentPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿
+using MyMobileApp.Models;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyMobileApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CoffeeEquipmentPage : ContentPage
+    {
+        public CoffeeEquipmentPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void MenuItem_Clicked(object sender, System.EventArgs e)
+        {
+            var coffee = ((MenuItem)sender).BindingContext as Coffee;
+            if (coffee == null)
+                return;
+
+            await DisplayAlert("Фаворизирано кафе", coffee.Name, "Ok");
+
+        }
+
+        private async void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var coffee = ((ListView)sender).SelectedItem as Coffee;
+            if (coffee == null)
+                return;
+
+            await DisplayAlert("Избрано кафе", coffee.Name, "Ok");
+        }
+
+        private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+           // ((ListView)sender).SelectedItem = null;
+        }
+    }
+}
